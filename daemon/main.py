ae27a1895346b94c232660c97a7ae12163f1e4a9
20alexl <alisenko2207@gmail.com>
2024-03-19 22:23:38 -0400

    Error receiving images

--- daemon/main.py
+++ daemon/main.py
@@ -1,28 +1,25 @@
 #main controller for Pi (daemon)
+import sys
 
-from distutils.cmd import Command
-from re import S
-from tkinter import SE
 from camera.camera_controller import ThermalCamera, BasicUSBcamera
 from communication_server.server import CommunicationServer
 
 #golbal variables
 host = "192.168.10.174"  # Raspberry Pi IP address
 port = 12345  # Chosen port number
-cam_top = 0
-top = 1
-side = 2
+cam1 = 0
+therm1 = 1
+therm2 = 2
 
 def main_init():
-    cam1 = BasicUSBcamera(cam_top)
-    therm1 = ThermalCamera(top)
-    therm2 = ThermalCamera(side)
+    cam1 = BasicUSBcamera(cam1)
+    #therm1 = ThermalCamera(therm1)
+    #therm2 = ThermalCamera(therm2)
     printer = None
     server = CommunicationServer(host, port, cam1, therm1=None, therm2=None, printer=None)
     return server
 
 if __name__ == "__main__":
     server = main_init()
-    if None:
-        server.start_server()
+    server.start_server()
     
